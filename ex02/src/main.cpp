3e59b7c1f660818059263c139d7e2bb82111b74a
Nigel Robinson <nrobinso@paul-f4Br9s2.clusters.42paris.fr>
2025-01-12 14:52:19 +0100

    ex02 WIP almost done

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 10:06:58 by nrobinso          #+#    #+#             */
-/*   Updated: 2025/01/12 14:49:19 by nrobinso         ###   ########.fr       */
+/*   Updated: 2025/01/12 14:50:47 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,6 @@ int main( void ) {
     FragTrap fragname("FragName");
     ScavTrap claptrap("nigel");
     ScavTrap noname;
-    // ScavTrap test;
     
     displayAllVariables(claptrap);
     displayAllVariables(fragname);
@@ -55,7 +54,6 @@ int main( void ) {
     claptrap.guardGate();
     fragname.highFivesGuys();
     fragnoname.highFivesGuys();
-    // std::cout << test.getName();
    
     hitRepair(claptrap, 1);
 
